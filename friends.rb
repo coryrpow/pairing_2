cd078cfcf8025a64832df7ea0073ed4026a29021
Cory Powell <coryrpowell@gmail.com>
2023-05-16 21:15:00 -0400

    Add places

--- friends.rb
+++ friends.rb
@@ -3,3 +3,12 @@ arrays = ["Mike", "Tim", "Jerry", "Ryan", "Peggy"]
 puts arrays [2]
 puts arrays [4]
 puts arrays [0]
+
+places = ["Denver", "Boulder", "Philly", "Las Vegas", "New York"]
+puts places.pop 
+puts places.pop
+puts places.pop
+puts places.pop
+puts places.pop
+puts places.pop
+
